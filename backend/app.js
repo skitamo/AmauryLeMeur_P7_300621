0f4e003bf94150024a8c55806c8e3a40547efead
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-30 10:21:21 +0200

    Réalisation de app.js

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+/* Importation des modules */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+/* Importation des modules de sécurité */
+const helmet = require('helmet');
+const session = require('cookie-session');
+
+/* Déclaration des routes */
+
+
+/* Création d'une application express*/
+const app = express();
+
+/* Module permettant de stocker des informations sensibles séparément du code */
+require('dotenv').config();
+
+/* Connection à la base de données */
+
+
+/* Middleware Header pour éviter les erreurs CORS */
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+});
+
+/* Middleware permettant la sécurisation des cookies */
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000);
+app.use(session({
+	name: 'sessionId',
+	secret: process.env.KEY_SESSION,
+	cookie: {
+		secure: true,
+		httpOnly: true,
+		domain: 'http://localhost:3000',
+		expires: expiryDate
+	}
+}));
+
+/* Middleware permettant de parser les requêtes envoyées par le client. On peut y accéder grâce à req.body */
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+/* Middleware permettant de protéger l'application de certaines des vulnérabilités bien connues du Web en configurant de manière appropriée les en-têtes HTTP */
+app.use(helmet());
+
+/* Utilisation de bodyParser pour transformer les données arrivant de la requête POST en objet JSON */
+app.use(bodyParser.json());
+
+/* Middleware permettant de charger les fichiers qui sont dans le dossier "images" */
+app.use('/images/', express.static(path.join(__dirname, 'images')));
+
+/* Middlewares permettant de transmettre les requêtes vers les routes correspondantes */
+
+
+/* Exportation de l'application */
+module.exports = app;
